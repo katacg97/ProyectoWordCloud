04c2a459b642af52a7fcec94374ae713bcb7d48a
katacg97 <katacg97@gmail.com>
2021-07-22 01:05:38 -0600

    Se agrego una clase para threads
    Se agrego una clase para implementar los threads.

--- Main.py
+++ Main.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 from pandas import DataFrame
 from collections import Counter
+import threading
+import time
 nltk.download('stopwords')
 
 nlp = spacy.load("es_core_news_sm") 
@@ -103,6 +105,23 @@ class archivo ():
          
         plt.show()
         
+class myThread (threading.Thread):
+    
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+      
+    def run(self):
+        print ("Starting " + self.name)
+        #print_time(self.name, 5, self.counter)
+        
+        print ("Exiting " + self.name)   
+        
+        
+        
+        
         
         
 texto = archivo("peterpan.pdf", 'DocModificable.txt')
@@ -121,6 +140,20 @@ texto.contador()
 print('/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////')
 print('\nFrecuencia de palabras\n')
 print(texto.frecuencia)
-print('Data Frame')
+
+
+# Create new threads
+thread1 = myThread(1, "Parte 1", 1)
+thread2 = myThread(2, "Parte 2", 2)
+
+# Start new Threads
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
+
+
+#print('Data Frame')
 #texto.lista_a_DataFrame()
 texto.Word_Cloud()
